1e14847badfcd66ff0f428341510af4b5882c999
NSSSSVODF23 <max95202@gmail.com>
2023-05-02 18:00:39 +0300

    Инструкции для запуска

---
+++ deploy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+cd /root/crm/microelCRM
+git pull
+yes | cp /root/crm/microelCRM/crm.service /etc/systemd/system/
+systemctl daemon-reload
+systemctl enable crm
+systemctl restart crm
---
+++ run.sh
@@ -0,0 +1,1 @@
+java -Xms100M -Xmx500M -jar /root/crm/microelCRM/target/microelcrm-0.0.1-SNAPSHOT.jar
