31bb6e5eed7baf2c97caa8c6941f970606fc9d51
NSSSSVODF23 <max95202@gmail.com>
2023-05-03 10:39:16 +0300

    Изменил файл развертки

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd /root/crm/microelCRM
 git pull
-yes | cp /root/crm/microelCRM/crm.service /etc/systemd/system/
+yes | cp /root/crm/microelCRM/crm.service /etc/systemd/system/crm.service
 systemctl daemon-reload
 systemctl enable crm
 systemctl restart crm
